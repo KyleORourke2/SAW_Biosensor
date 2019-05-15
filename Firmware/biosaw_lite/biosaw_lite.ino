51af6f25fe77d27e59c08296c36bcf01cc619bdb
Kyle O'Rourke <ktorourk@ucsc.edu>
2019-05-15 11:20:57 -0700

    Dropped libarary because it was not working or we could not figure out how to use it.

--- Firmware/biosaw_lite/biosaw_lite.ino
+++ Firmware/biosaw_lite/biosaw_lite.ino
@@ -1,52 +1,45 @@
 // biosaw_lite.ino
-// Plan B biosaw code
+// Biolite Firmware
 
 // Written by: Kyle O'Rourke & Jackson Atwater
 // 5/14/2019
 // UCSC SDP
 
-#include <AD9959.h>
 #include <SPI.h>
 
-const int red_led = 3;
-const int green_led = 5;
-const int blue_led = 6;
+#define CSR 0x00
+#define FR1 0x01
+#define FR2 0x02
+#define CFR 0x03
+#define CFW 0x04
 
 const int CS = 10;       // Chip select
 const int rst_dds = 2;   // DDS reset pin.
 const int io_update = 4; // DDS I/O update toggle.
 const int P0 = 8;        // Sweep control pin
 
-class MyAD9959 : public AD9959<
-    rst_dds,        // Reset pin (active = high)
-    CS,             // Chip Enable (active = low)
-    io_update,      // I/O_UPDATE: Apply config changes (pulse high)
-    25000000,       // 25MHz crystal (optional)
-    1000000         // SPI rate 1MBit/s
-> {};
-MyAD9959  dds;
+const int red_led = 3;   // RGB LED
+const int green_led = 5;
+const int blue_led = 6;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Startup...");
+  Serial.begin(115200);
+  Serial.println("BioLite Startup...");
 
+  // Set pinModes:
   pinMode(CS, OUTPUT);
   pinMode(rst_dds, OUTPUT);
   pinMode(io_update, OUTPUT);
   pinMode(P0, OUTPUT);
+  pinMode(red_led, OUTPUT);
+  pinMode(green_led, OUTPUT);
   pinMode(blue_led, OUTPUT);
-  delay(500); // Wait for all devices to turn on.
-  digitalWrite(blue_led, HIGH);
-  
-  // Check DDS.
-  dds.reset();
-  dds.setClock(20, 0);
-  delay(10);
-  uint32_t  delta;
-  delta = dds.frequencyDelta(455000);
-  delay(10);
-  dds.setFrequency(MyAD9959::Channel3, 10000000UL);
-  dds.update();
+
+  // Set init pin values:
+  leds(0, 20, 10); // Turn on LED for visual verification.
+  digitalWrite(P0, LOW);  // Ensure profile pin 0 low.
+  digitalWrite(CS, HIGH); // Ensure slave select high
+  SPI.begin();
 
   
   Serial.println("Startup Finished.");
@@ -54,5 +47,82 @@ void setup() {
 
 void loop() {
 
-  delay(100);
+  
+}
+
+
+// Set control registers. From Pg.36 Table 28.
+int dds_setup(){
+  
+  // Channel Select Register:
+  ddsWrite_8(CSR, 0x82); // Set Ch.3 EN, I/O mode 3-wire is 01
+                         // (10000010)
+  
+  // Function Register 1:
+  ddsWrite_24(FR1, 0x00D00020);
+  // VCO gain[7], PLL divider[6:2], Charge pump[1:0]
+  // Open[7], PPC[6:4], RU/RD[3:2], Mod[1:0]
+  // RefClk[7], ExtPwrDwn[6], SYNC_CLK disable[5], ...
+
+  // Channel Function Register:
+  ddsWrite_24();
+}
+
+
+
+// Simple function for writing 8 bit values
+// 1 byte = 8 bits (Arduino)
+// 1 hex val = 4 bits
+void ddsWrite_8(byte address, byte val){
+  spiBegin();
+  SPI.transfer(address);
+  SPI.transfer(val);
+  spiEnd();
+}
+
+// Simple function for writing 16 bit values
+void ddsWrite_16(byte address, int val){
+  spiBegin();
+  SPI.transfer(address);
+  SPI.transfer(&val, 2);
+  spiEnd();
+}
+
+// Simple function for writing 3 byte values
+void ddsWrite_24(byte address, long val){
+  byte a,b,c;
+  a=(val      &0xFF); //extract first byte
+  b=((val>>8) &0xFF); //extract second byte
+  c=((val>>16)&0xFF); //extract third byte
+  
+  spiBegin();
+  SPI.transfer(address);
+  SPI.transfer(c); // We want to transfer MSB first.
+  SPI.transfer(b);
+  SPI.transfer(a);
+  spiEnd();
+}
+
+// Simple function for writing 4 byte values
+void ddsWrite_32(byte address, long val){
+  spiBegin();
+  SPI.transfer(address);
+  SPI.transfer(&val, 4);
+  spiEnd();
+}
+
+void spiBegin(){
+  digitalWrite(CS, LOW);  // Enable slave select.
+  delay(1);               // Wait for chip to enable.
+}
+
+void spiEnd(){
+  digitalWrite(CS, HIGH);  // Disable slave select.
+}
+
+// Simple LED control function
+void leds(int R, int G, int B) {
+  analogWrite(red_led, 255-R);
+  analogWrite(green_led, 255-G);
+  analogWrite(blue_led, 255-B);
 }
