b2709de00e27220bd6aeda48b1c4824c4983942e
Kyle O'Rourke <ktorourk@ucsc.edu>
2019-05-22 14:08:31 -0700

    Updated arduino Vref settings.

--- Firmware/biosaw_lite/biosaw_lite.ino
+++ Firmware/biosaw_lite/biosaw_lite.ino
@@ -31,9 +31,11 @@ const unsigned long CFR_V = 0x00000302; // Set DAC FS current, Clear Phase accum
 unsigned long CFTW_V = 0;  // Frequency tuning word. 32-bits.
 
 // ADC VARIABLES:
-#define voltMul 0.00322265625 // Value of (3.3)/(2^10)
+#define pwrSlope 0.0199 // mV/dB 
+#define ext_vref 1.939  // Voltage on external voltage ref.
+const float voltMul = (ext_vref)/(1024); // Value of (ext_vref)/(2^10) Shoul be: 0.0018935546875
 
-// PIN DEFINITIONS:
+// PIN DEFINITIONS:1
 const int rst_dds = 2;   // DDS reset pin.
 const int io_update = 4; // DDS I/O update toggle.
 const int P0 = 8;        // Sweep control pin
@@ -56,6 +58,8 @@ void setup() {
   // SERIAL SETUP:
   Serial.begin(115200); // USER'S COMPUTER SERIAL COM SETUP (NOT SPI)
   Serial.print("BioLite Startup...\n");
+  Serial.print("voltMul = "); Serial.print(voltMul, 9);
+  Serial.print("  Should be: "); Serial.print("0.0018935546875\n");
   Serial.print("FTW_CFB = "); Serial.print(FTW_CFB, 9);
   Serial.print("  Should be: "); Serial.print("0.11641532182\n");
   Serial.print("FTW_CBF = "); Serial.print(FTW_CBF, 8);
@@ -75,9 +79,9 @@ void setup() {
   pinMode(ADC_2,      INPUT);
 
   // SET PIN INITIAL VALUES:
-  dleds(0,0,0);
   digitalWrite(P0, LOW); // Ensure profile pin 0 low.
   digitalWrite(CS, LOW); // Ensure slave select high to start.
+  startupLEDS();         // Little LED show for startup.
   
   // SPI STARTUP MUST OCCUR AFTER PIN DEFINITIONS & BEFORE DDS SETUP:
   SPI.begin();
@@ -85,24 +89,41 @@ void setup() {
   // DDS SET INITIAL REGISTER VALUES:
   dds_setup();
 
+  // ADC REFERENCE SETUP:
+  analogReference(EXTERNAL);
+
   // SETUP FINISHED:
   Serial.println("Startup Finished.");
 }
 
 // MAIN LOOP:
 void loop() {
-  for(int i = 0; i < 250; i++){
+  setFreqMhz(10);
+  for(int i = 1; i < 100; i++){
     setFreqMhz(i);
-    printADCs();
-    delay(20);
+    delay(100);
+    int adc1_in = analogRead(ADC_1); // ref
+    int adc2_in = analogRead(ADC_2); // test
+    Serial.print("ADC: "); Serial.print(adc1_in); Serial.print("  ADC2: "); Serial.println(adc2_in);
+    int adc_dif = adc2_in - adc1_in;
+    float dif_vol = convVol(adc_dif);
+    float dB_in   = convPwr(dif_vol);
+    //Serial.print("dB: "); Serial.print(dB_in, 5); Serial.print("  Freq (Mhz): "); Serial.println(i);
   }
-  
+  Serial.println("SWEEP END");
+  delay(100000);
+
 }
 
-// ABSOLUTELY THE WORST ADC FUNCTION POSSIBLE.
-void printADCs(){
-  Serial.print("ADC Ref: ");  Serial.print(float(analogRead(ADC_1)*voltMul), 4);
-  Serial.print(" ADC Tst: "); Serial.println(float(analogRead(ADC_2)*voltMul), 4);
+// CONVERT VOLTAGE READING TO OUTPUT POWER:
+float convPwr(float voltage){
+  return (voltage/pwrSlope);
+}
+
+// CONVERT ADC VALUE TO VOLTAGE:
+// RETURNS VOLTAGE AS A FLOAT
+float convVol(int adc_val){
+  return float(adc_val*voltMul);
 }
 
 // SETS THE OUTPUT OF THE DDS TO THE GIVEN FREQUENCY
@@ -114,6 +135,10 @@ void setFrequency(float frequency){
 
 void setFreqMhz(float frequency){
   setFrequency(frequency*1000000);
+}
+
+void setFreqkHz(float frequency){
+  setFrequency(frequency*1000);
 }
 
 // Calculates the needed FTW for a desired frequency output:
@@ -228,4 +253,116 @@ void dleds(int R, int G, int B) {
   digitalWrite(red_led, 1-R);
   digitalWrite(green_led, 1-G);
   digitalWrite(blue_led, 1-B);
+}
+
+// Cute little LED start function:
+void startupLEDS(){
+  for(int i = 0; i < 255; i++){
+    leds(0,0,i);
+    delay(3);
+  }
+  for(int i = 0; i < 255; i++){
+    leds(0,i,255-i);
+    delay(2);
+  }
+  for(int i = 255; i > 0; i--){
+    leds(0,i,0);
+    delay(2);
+  }
+  dleds(0,0,0);
+}
+
+//Looks for incoming serial information and if found, goes into diagnostics loop. 
+/*
+void serialControl(){
+	if(Serial.available() > 0){		//Checks for serial data.
+		while(true){
+			if(Serial.available() > 0){
+				//Read the incoming.
+        String incoming = Serial.readString();
+        //Echo incoming.
+        Serial.println();
+        Serial.print("Received: ");
+        Serial.println(incoming);
+        incoming.toLowerCase();
+        //Actions:
+        if(incoming == "diagnostics" || incoming == "info"){
+          print_diagnostics();
+        }
+        else if(incoming == "servo test"){
+          leds_off();
+          servo.attach(servo_pin);
+					green(0);
+					servo.write(settings.servo_release_pos);
+					delay(1000);
+
+					green(150);
+					red(0);
+					servo.write(settings.servo_mid_pos);
+					delay(1000);
+
+					green(255);
+					servo.write(settings.servo_lock_pos);
+					delay(1000);
+
+					leds_off();
+					servo.detach();
+        }
+        else if(incoming == "exit"){
+          leds_off();
+          break;
+        }
+        else if(incoming == "help"){
+          Serial.println();
+          Serial.println("Valid commands:");
+          Serial.println("1) diagnostics or info");
+          Serial.println("2) arm");
+          Serial.println("3) servo test");
+          Serial.println("4) exit");
+          Serial.println("(Not Case sensitive.)");
+        }
+        else{
+          Serial.println();
+          Serial.println("Command not recognized! Type 'help' for more info.");
+        }
+	    }
+
+	    //Fade blue while in Serial loop.
+      for(int i = 0; i != 255; i++){
+				leds(0,0,i);
+				delay(4);
+			}
+			for(int i = 255; i != 0; i--){
+				leds(0,0,i);
+				delay(4);
+			}
+		}
+	}
+}
+*/
+
+float readVcc(){
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+
+  float result = (high<<8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
 }
